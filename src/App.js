7a55138e22e88bdae17aa8d9c6eac6b1766c8b70
Akhila H <Akhilah@er.ai>
2022-11-07 20:51:01 +0530

    about us

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./flex.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Us</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+        ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+        Lorem ipsum Nam liber tempor cum soluta nobis eleifend option congue
+        nihil imperdiet doming id quod mazim placerat facer possim assum. Typi
+        non habent claritatem insitam; est usus legentis in iis qui facit eorum
+        claritatem. Lorem ipsum Duis autem vel eum iriure dolor in hendrerit in
+        vulputate velit esse molestie consequat, vel illum dolore eu feugiat
+        nulla facilisis at vero eros et accumsan. Lorem ipsum dolor sit amet,
+        consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+        laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
+        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+        aliquip ex ea commodo consequat. Lorem ipsum Nam liber tempor cum soluta
+        nobis eleifend option congue nihil imperdiet doming id quod mazim
+        placerat facer possim assum. Typi non habent claritatem insitam; est
+        usus legentis in iis qui facit eorum claritatem. Lorem ipsum Duis autem
+        vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+        consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
+        accumsan.
+      </p>
     </div>
   );
 }
